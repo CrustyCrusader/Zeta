0994c5dad5783f4cfe5097a619e5351a319729ee
CrustyCrusader <christopher.cruz.2015@gmail.com>
2022-12-14 09:04:35 -0500

    url fix

--- Artemis/urls.py
+++ Artemis/urls.py
@@ -13,23 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import include, path
 
 from pages.views import home_view, contact_view, about_view
-from products.views import (
-    product_detail_view, 
-    product_delete_view,
-    product_create_view, 
-    product_list_view,
-    render_initial_data,
-    dynamic_lookup_view
-    )
-
-urlpatterns = [
-    path('products/', product_list_view, name='product-list'),
-    path('prdocuts/<int:id>/', dynamic_lookup_view, name='product-detail'),
-    path('products/<int:id>/delete/', product_delete_view, name='product-delete'),
-
-
 
 urlpatterns = [
     path('products/', include('products.urls')),

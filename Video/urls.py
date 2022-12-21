c06b6ec7c2e4734254756a66a9bc585963579294
CrustyCrusader <christopher.cruz.2015@gmail.com>
2022-12-20 22:49:53 -0500

    Video edit

--- Video/urls.py
+++ Video/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
-from .views import (upload_video)
+from .views import (upload_video,
+   VideoDeleteView,
+   VideoDetailView,
+   VideoListView,
+   VideoUpdateView,
+   
 
+)
+
+    
+  
 
 app_name ='Video'
 urlpatterns = [
-
+    path('',VideoListView.as_view(), name='video-list'),
     path('upload/', upload_video, name='upload_video'),
-
+    path('<int:id>/',VideoDetailView.as_view(), name='video-detail'),
+    path('<int:id>/update/',VideoUpdateView.as_view(), name='video-update'),
+    path('<int:id>/delete/',VideoDeleteView.as_view(), name='video-delete'),
 
 ]
--- Video/views.py
+++ Video/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.generic import (
+    DetailView,
+    ListView,
+    UpdateView,
+    ListView,
+    DeleteView
+)
+
 from .forms import VideoForm
-# Create your views here
+from .models import Video
+
 
 def upload_video(request):
     if request.method == 'POST':
@@ -11,3 +21,40 @@ def upload_video(request):
     else:
         form = VideoForm()
     return render(request, 'upload_video.html', {'form': form})
+
+class VideoListView(ListView):
+    template_name = 'Video_list.html'
+    queryset = Video.objects.all()
+
+class VideoDetailView(DetailView):
+        template_name = 'video_detail.html'
+    #queryset = Video.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Video, id=id_)
+
+
+class VideoUpdateView(UpdateView):
+    template_name = 'video_create.html'
+    form_class = VideoForm
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Video, id=id_)
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+
+class VideoDeleteView(DeleteView):
+    template_name = 'video_delete.html'
+    
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Video, id=id_)
+
+    def get_success_url(self):
+        return reverse('Videos:Video-list')
+

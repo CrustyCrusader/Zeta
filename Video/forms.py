acf5218f6b7fa5027d7c9b71bbcc51cbbdf7e8a7
CrustyCrusader <christopher.cruz.2015@gmail.com>
2023-04-21 17:57:36 -0400

    fixed IndentationError

--- Video/forms.py
+++ Video/forms.py
@@ -4,7 +4,7 @@ from .models import Video
 class VideoForm(forms.ModelForm):
      title = forms.CharField(max_length=200)
      video_file = forms.FileField()
-    class Meta:
+     class Meta:
         model= Video
         fields = [
         'title',

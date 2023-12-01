162c82482ef2eed8cb3c2098f6682bcee2c7fa87
CrustyCrusader <christopher.cruz.2015@gmail.com>
2023-12-01 02:48:06 -0500

    get

--- Video/models.py
+++ Video/models.py
@@ -6,8 +6,8 @@ from django.core.validators import FileExtensionValidator
 class Video(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    video_file = models.FileField(upload_to='media/Video', validators = [FileExtensionValidator(allowed_extensions=['mp4'])])
-    thumbnail = models.FileField(blank = True, null = True, upload_to='media/thumbnails', validators = [FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])])
+    video_file = models.FileField(upload_to='Video/', validators = [FileExtensionValidator(allowed_extensions=['mp4'])])
+    thumbnail = models.FileField(blank = True, null = True, upload_to='thumbnails/', validators = [FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])])
     date_posted = models.DateTimeField(default=timezone.now)
     # Add other fields as needed
     def get_absolute_url(self):
